e262b920e5eef713e5145aa5b427c2c9f79d3cb3
Kyle Burnette <kyle@kyleburnette.com>
2021-03-28 16:36:10 -0700

    Added randomly generated images appearing when you click on a button

--- public/scripts/game_settings.js
+++ public/scripts/game_settings.js
@@ -4,4 +4,4 @@ var pollingRate = 1000; //ms
 var growthRate = 0.15;
 
 stepsPerSecond = {skateboard: 1, bicycle: 2, car: 5, train: 8, plane: 9, spaceship: 10};
-basePrices = {skateboard: 10, bicycle: 200, car: 500, train: 1000, plane: 5000, spaceship: 10000};
+basePrices = {skateboard: 0, bicycle: 200, car: 500, train: 1000, plane: 5000, spaceship: 10000};
