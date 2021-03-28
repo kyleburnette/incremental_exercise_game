d03bef499e07d45691153e60c3494132b6153b6b
Kyle Burnette <kyle@kyleburnette.com>
2021-03-28 13:44:23 -0700

    Refactored functions

---
+++ public/scripts/game.js
@@ -0,0 +1,267 @@
+var inventory = { bicycle: 0, car: 0, plane: 0, skateboard: 0, spaceship: 0, train: 0 };
+
+function retrieveUserScore() {
+    var user = firebase.auth().currentUser;
+    var score = db.collection("scores").doc(user.uid);
+    console.log("Logged ID:", user.uid);
+    score.get().then((doc) => {
+        if (doc.exists) {
+            userScore = parseInt(doc.data()["score"]);
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
+}
+
+function calcTotalStepsPerSecond(){
+    return 1
+    + inventory.skateboard * stepsPerSecond.skateboard 
+    + inventory.bicycle * stepsPerSecond.bicycle 
+    + inventory.car * stepsPerSecond.car
+    + inventory.train * stepsPerSecond.train
+    + inventory.plane * stepsPerSecond.plane
+    + inventory.spaceship * stepsPerSecond.spaceship
+}
+
+function updateCounts() {
+    $("#skateboard-count").html("Skateboards: " + inventory.skateboard);
+    $("#bicycle-count").html("Bicycles: " + inventory.bicycle);
+    $("#car-count").html("Cars: " + inventory.car);
+    $("#train-count").html("Trains: " + inventory.train);
+    $("#plane-count").html("Planes: " + inventory.plane);
+    $("#spaceship-count").html("Spaceships: " + inventory.spaceship);
+    
+    $("#skateboard-info").html("+" + inventory.skateboard * stepsPerSecond.skateboard + " steps/second");
+    $("#bicycle-info").html("+" + inventory.bicycle * stepsPerSecond.bicycle + " steps/second");
+    $("#car-info").html("+" + inventory.car * stepsPerSecond.car + " steps/second");
+    $("#train-info").html("+" + inventory.train * stepsPerSecond.train + " steps/second");
+    $("#plane-info").html("+" + inventory.plane * stepsPerSecond.plane + " steps/second");
+    $("#spaceship-info").html("+" + inventory.spaceship * stepsPerSecond.spaceship + " steps/second")
+
+    $("#skateboard-cost").html(Math.round(calcGrowth(basePrices.skateboard, inventory.skateboard)));
+    $("#bicycle-cost").html(Math.round(calcGrowth(basePrices.bicycle, inventory.bicycle)));
+    $("#car-cost").html(Math.round(calcGrowth(basePrices.car, inventory.car)));
+    $("#train-cost").html(Math.round(calcGrowth(basePrices.train, inventory.train)));
+    $("#plane-cost").html(Math.round(calcGrowth(basePrices.plane, inventory.plane)));
+    $("#spaceship-cost").html(Math.round(calcGrowth(basePrices.spaceship, inventory.spaceship)));
+
+    $("#total-steps-per-second").html("Total steps per second: " + calcTotalStepsPerSecond());
+    $("#debug").html(`Total Steps: ` + Math.round(userScore));
+}
+
+function retrieveUserInventory() {
+    var user = firebase.auth().currentUser;
+    var inventoryDB = db.collection("inventory").doc(user.uid);
+    console.log("Logged ID:", user.uid);
+    inventoryDB.get().then((doc) => {
+        if (doc.exists) {
+            inventory.bicycle = parseInt(doc.data()["bicycle"]);
+            inventory.car = parseInt(doc.data()["car"]);
+            inventory.plane = parseInt(doc.data()["plane"]);
+            inventory.skateboard = parseInt(doc.data()["skateboard"]);
+            inventory.spaceship = parseInt(doc.data()["spaceship"]);
+            inventory.train = parseInt(doc.data()["train"]);
+
+            updateCounts();
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
+}
+
+function writeUserScore(text) {
+    var updateScore = db.collection("scores");
+    userScore = Math.round(userScore);
+    if (loggedInUser == null) {
+        console.warn("User is not logged in!");
+    } else {
+        updateScore.doc(loggedInUser.uid).set({
+            score: userScore
+        }, {
+            merge: true
+        });
+    }
+}
+
+function writeUserInventory(text) {
+    var updateInv = db.collection("inventory");
+    if (loggedInUser == null) {
+        console.warn("User is not logged in!");
+    } else {
+        updateInv.doc(loggedInUser.uid).set({
+            skateboard: inventory.skateboard,
+            bicycle: inventory.bicycle,
+            car: inventory.car,
+            train: inventory.train,
+            plane: inventory.plane,
+            spaceship: inventory.spaceship,
+        }, {
+            merge: true
+        });
+    }
+}
+
+var previousCrd = null;
+var distanceThisCycle = 0;
+var options = {
+    enableHighAccuracy: true,
+    timeout: 2000,
+    maximumAge: 0
+};
+
+function calcDistance(previousCrd, currentCrd) {
+    const R = 6371e3; // meters
+    const φ1 = previousCrd.latitude * Math.PI / 180; // φ, λ in radians
+    const φ2 = currentCrd.latitude * Math.PI / 180;
+    const Δφ = (currentCrd.latitude - previousCrd.latitude) * Math.PI / 180;
+    const Δλ = (currentCrd.longitude - previousCrd.longitude) * Math.PI / 180;
+
+    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    distanceThisCycle = R * c;
+    return d = R * c; // in meters
+}
+
+function travelSpeed() {
+    return distanceThisCycle / (pollingRate / 1000); //meters per second
+}
+
+function logOut() {
+    firebase.auth().signOut()
+        .then(function () {
+            window.location.href = "index.html";
+        })
+        .catch(function (error) {
+            // An error happened
+        });
+}
+
+function isWalking() {
+    return travelSpeed() > 1.5 //meters per second
+}
+
+function handleUserScore() {
+    userScore += 1
+        + (inventory.bicycle * stepsPerSecond.bicycle)
+        + (inventory.skateboard * stepsPerSecond.skateboard)
+        + (inventory.car * stepsPerSecond.car)
+        + (inventory.spaceship * stepsPerSecond.spaceship)
+        + (inventory.train * stepsPerSecond.train)
+        + (inventory.plane * stepsPerSecond.plane);
+}
+
+function success(pos) {
+    var crd = pos.coords;
+    if (previousCrd != null) {
+        distance = calcDistance(previousCrd, crd);
+        console.log("Distance:", distance);
+        if (crd.accuracy > 25) {
+            console.log("Accuracy:", crd.accuracy);
+            console.log("Inaccurate, did not take position.");
+        }
+        if (isWalking()) {
+            document.getElementById("walking-indicator").innerHTML = "You are walking and earning points!"
+            handleUserScore();
+            distance = calcDistance(previousCrd, crd);
+        } else {
+            handleUserScore();
+            document.getElementById("walking-indicator").innerHTML = "You aren't walking!";
+        }
+    }
+    
+    updateCounts();
+    previousCrd = pos.coords;
+}
+
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+function calcGrowth(base, x) {
+    return base * Math.pow((1 + growthRate), x)
+}
+
+loggedInUser = null;
+var userScore = 0;
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        loggedInUser = user;
+        console.log("Logged in as", loggedInUser.displayName);
+        retrieveUserScore();
+        retrieveUserInventory();
+    } else {
+        console.warn("No user detected!");
+    }
+});
+
+var intervalId = window.setInterval(function () {
+    navigator.geolocation.getCurrentPosition(success, error, options); 
+}, pollingRate);
+
+var updateScoreTimer = window.setInterval(function () {
+    writeUserScore();
+    writeUserInventory();
+}, updateRate);
+
+function getPropertyName(id){
+    switch(id){
+        case "skateboard-button":
+            return "skateboard";
+        case "bicycle-button":
+            return "bicycle";
+        case "car-button":
+            return "car";
+        case "train-button":
+            return "train";
+        case "plane-button":
+            return "plane";
+        case "spaceship-button":
+            return "spaceship";
+    }
+}
+
+function increaseInventoryCount(inventoryType){
+    switch(inventoryType){
+        case "skateboard":
+            inventory.skateboard++;
+            break;
+        case "bicycle":
+            inventory.bicycle++;
+            break;
+        case "car":
+            inventory.car++;
+            break;
+        case "train":
+            inventory.train++;
+            break;
+        case "plane":
+            inventory.plane++;
+            break;
+        case "spaceship":
+            inventory.spaceship++;
+            break;
+        default:
+            break;
+    }
+    updateCounts();
+}
+
+$(".game-button").click(function(){
+    buttonType = $(this).attr("id");
+    inventoryType = getPropertyName(buttonType);
+    
+    price = calcGrowth(basePrices[inventoryType], inventory[inventoryType]);
+    if (userScore >= price){
+        userScore -= price;
+        increaseInventoryCount(inventoryType);
+    }
+});
---
+++ public/scripts/game_settings.js
@@ -0,0 +1,7 @@
+var updateRate = 5000; //ms
+var pollingRate = 1000; //ms
+
+var growthRate = 0.15;
+
+stepsPerSecond = {skateboard: 1, bicycle: 2, car: 5, train: 8, plane: 9, spaceship: 10};
+basePrices = {skateboard: 10, bicycle: 200, car: 500, train: 1000, plane: 5000, spaceship: 10000};
