08e57a052abb41f14f18a8d0cd891548eadebbb0
Kyle Burnette <kyle@kyleburnette.com>
2021-03-29 17:42:36 -0700

    Turned on debug text output for testing

--- public/scripts/game_settings.js
+++ public/scripts/game_settings.js
@@ -5,7 +5,7 @@ var minAccuracy = 5; //meters
 var growthRate = 0.15;
 
 var debug = false;
-var textDebug = false;
+var textDebug = true;
 
 stepsPerSecond = {skateboard: 1, bicycle: 2, car: 5, train: 8, plane: 9, spaceship: 10};
 basePrices = {skateboard: 10, bicycle: 100, car: 500, train: 1000, plane: 5000, spaceship: 10000};
