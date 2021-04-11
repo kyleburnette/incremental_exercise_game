dec1c503f3268f8c568bdc16c35fa3d6b520ed02
Alex Wong <wongalexjx@gmail.com>
2021-04-10 17:16:45 -0700

    added name,score to leaderboard

--- public/scripts/goal.js
+++ public/scripts/goal.js
@@ -11,42 +11,41 @@ function scoreQuery() {
                 var highScore = doc.data().score;
                 var div = document.getElementById('leaderboard');
                 var tbl = document.createElement('table');
-                var highScoreString = highScore ;
+                var highScoreString = highScore;
                 tbl.style.width = '100%';
-                var header = tbl.createTHead();
-                var row = header.insertRow(0); 
+                //var header = tbl.createTHead();
+                //var row = header.insertRow(0);
                 //tbl.style.maxWidth = "100 px";
                 //tbl.style.borderBottom = '1px solid black';
                 tbl.className = "table table-bordered ";
-                
-/*                 for (var j = 0; j < 3; j++) {
-                    var tk = tbl.insertRow(0);
-                } */
-                //rows
-                for (var i = 0; i < 1; i++) 
+
+
+                for (var i = 0; i < 1; i++) {
                     var tr = tbl.insertRow();
                     var td = tr.insertCell();
                     var tk = tr.insertCell();
+                }
 
-                        // values              
-                        for (var k = 0; k < 1; k++){
-                        if (i == 0 && k == 0) {
-                            break;
-                        } else {
-                            //var tk = tk.insertCell();
-                            tk.append(highScoreString);
-                            td.append(n);
-                            //tk.append(highScore);
-                        }
-                        
+                // Values              
+                for (var k = 0; k < 1; k++) {
+                    if (i == 0 && k == 0) {
+                        break;
+                    } else {
+                        tk.append(highScoreString);
+                        td.append(n);
                     }
-                
+
+                }
+
                 div.appendChild(tbl);
+
 
             })
         })
 }
 scoreQuery();
+
+
 
 $(document).ready(function () {
     firebase.auth().onAuthStateChanged(function (user) {
@@ -117,23 +116,20 @@ function redeemGoalButton() {
 redeemGoalButton();
 
 // Displays Current Goal Count
-function displayGoal(){
+function displayGoal() {
     var userGoal = 0;
     var user = firebase.auth().currentUser;
     var stepGoals = db.collection("user").doc(user.uid);
-    var div = document.getElementById('currentGoal');
 
     stepGoals.get().then((doc) => {
         if (doc.exists) {
             userGoal = doc.data().goal;
-            //console.log(userGoal);
-            var userGoalString = " " + userGoal ;
-            //div.appendChild(userGoal);
+            var userGoalString = " " + userGoal;
             $("#goal-display").append(userGoalString + " Steps");
-               
+
         } else {
             $("#goal-display").append(userGoal + " Steps");
-            console.log(userGoal );
+            console.log(userGoal);
 
         }
     }).catch((error) => {
@@ -141,5 +137,3 @@ function displayGoal(){
     });
 }
 displayGoal;
-
-
--- public/scripts/upload_profilePic.js
+++ public/scripts/upload_profilePic.js
@@ -1,4 +1,5 @@
 const defaultPic = "https://firebasestorage.googleapis.com/v0/b/comp1800project.appspot.com/o/images%2Fprofile_pic.jpg?alt=media&token=fe2e2a67-f843-4b28-b83e-7516e584d689";
+
 firebase.auth().onAuthStateChanged(function (user) {
     var user = firebase.auth().currentUser;
     if (user != null) {
@@ -83,9 +84,9 @@ function defaultProfilePic() {
     //console.log("Logged ID:", user.uid);
     picture.get().then((doc) => {
         if (doc.exists) {
-            picUrl = doc.data().profilePic;      
+            picUrl = doc.data().profilePic;
             $("#mypic-goes-here").attr("src", picUrl);
-            console.log("picture loaded.")      
+            console.log("picture loaded.")
         } else {
             picUrl = defaultPic;
             console.log("No such document!");
@@ -153,81 +154,82 @@ function displayQuote() {
 }
 displayQuote();
 
-/* var fileInput = document.getElementById("mypic-input");
+var fileInput = document.getElementById("mypic-input");
 const preview = document.getElementById("preview-image");
 
 function uploadUserProfilePic() {
-  // Let's assume my storage is only enabled for authenticated users 
-  // This is set in your firebase console storage "rules" tab
+    // Let's assume my storage is only enabled for authenticated users 
+    // This is set in your firebase console storage "rules" tab
 
-  firebase.auth().onAuthStateChanged(function (user) {
+    firebase.auth().onAuthStateChanged(function (user) {
 
-      // listen for file selection
-      fileInput.addEventListener('change', function (e) {
-          var file = e.target.files[0];
-          var blob = URL.createObjectURL(file);
-          preview.src = blob;            // display this image
-          $("#showImage").modal("toggle");
-      })
-  })
+        // listen for file selection
+        fileInput.addEventListener('change', function (e) {
+            var file = e.target.files[0];
+            var blob = URL.createObjectURL(file);
+            preview.src = blob; // display this image
+            $("#showImage").modal("toggle");
+        })
+    })
 }
 uploadUserProfilePic();
 
 function confirmProfilePic() {
-  //store using this name
-  var storageRef = firebase.storage().ref("images/" + users.uid + ".jpg"); 
-            
-  //upload the picked file
-  storageRef.put(file) 
-      .then(function(){
-          console.log('Uploaded to Cloud Storage.');
-      })
+    //store using this name
+    var storageRef = firebase.storage().ref("images/" + users.uid + ".jpg");
 
-  //get the URL of stored file
-  storageRef.getDownloadURL()
-      .then(function (url) {   // Get URL of the uploaded file
-          console.log(url);    // Save the URL into users collection
-          db.collection("users").doc(user.uid).set({
-              "profilePic": url
-          },{
-            merge: true
-          })
-          .then(function(){
-              console.log('Added Profile Pic URL to Firestore.');
-          })
-      })
-      window.location.reload();
-} 
+    //upload the picked file
+    storageRef.put(file)
+        .then(function () {
+            console.log('Uploaded to Cloud Storage.');
+        })
+
+    //get the URL of stored file
+    storageRef.getDownloadURL()
+        .then(function (url) { // Get URL of the uploaded file
+            console.log(url); // Save the URL into users collection
+            db.collection("user").doc(user.uid).set({
+                    "profilePic": url
+                }, {
+                    merge: true
+                })
+                .then(function () {
+                    console.log('Added Profile Pic URL to Firestore.');
+                })
+        })
+    window.location.reload();
+}
 confirmProfilePic();
 
-function displayUserProfilePic() {
-    console.log("Displaying profile picture");
-    firebase.auth().onAuthStateChanged(function (user) {      //get user object
-        db.collection("users").doc(user.uid)                  //use user's uid
-            .get()                                            //READ the doc
-            .then(function (doc) {
-                var picUrl = doc.data().userPic;           //extract pic url
-                //if (profilePic === null){
-                  //console.log("profile pic is not null")
-                //}
-								// use this line if "mypicdiv" is a "div"
-                //$("#mypicdiv").append("<img src='" + picUrl + "'>")
-                
-								// use this line if "mypic-goes-here" is an "img" 
-								$("#mypic-goes-here").attr("src", profileUrl);
-            })
-    })
-}
-displayUserProfilePic();
+// function displayUserProfilePic() {
+//     console.log("Displaying profile picture");
+//     firebase.auth().onAuthStateChanged(function (user) { //get user object
+//         db.collection("users").doc(user.uid) //use user's uid
+//             .get() //READ the doc
+//             .then(function (doc) {
+//                 var picUrl = doc.data().userPic; //extract pic url
+//                 //if (profilePic === null){
+//                 //console.log("profile pic is not null")
+//                 //}
+//                 // use this line if "mypicdiv" is a "div"
+//                 //$("#mypicdiv").append("<img src='" + picUrl + "'>")
+
+//                 // use this line if "mypic-goes-here" is an "img" 
+//                 $("#mypic-goes-here").attr("src", profileUrl);
+//             })
+//     })
+// }
+// displayUserProfilePic();
 
 $(document).ready(function () {
-  firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-          loggedInUser = user;
-          console.log("Logged in as", loggedInUser.displayName);
-      } else {
-          console.warn("No user detected!");
-          window.location.href = "login.html";
-      }
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            loggedInUser = user;
+            console.log("Logged in as", loggedInUser.displayName);
+
+        } else {
+            console.warn("No user detected!");
+            window.location.href = "login.html";
+        }
     });
-}); */
+});
